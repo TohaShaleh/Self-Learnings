247813dbd2d8dab21b581786d47277ce2a1787bd
Abu Shaleh <abushalehtoha@gmail.com>
2024-08-04 23:43:32 +0600

    Added the code of Callback Hell problem

--- JavaScript/CallBack_Hell.js
+++ JavaScript/CallBack_Hell.js
@@ -8,11 +8,38 @@ function getdata(id,name,getnext){
             {
                 getnext();
             }
+        else
+        {
+            console.log("CallBack function has not been passed as argument ! ");
+        }
         
     }, 1300);
 }
 
-getdata(1,"Toha",function()   // This is the Callback function which is passed as argument to another function 
-  {
-       getdata(2,"Afrin")     // Here Callback function is not sent 
-  });
+//  Basic Callback function 
+//  getdata(31,"Toha",function()   // This is the Callback function which is passed as argument to another function 
+//    {
+//         getdata(20,"Badhon")     // Here Callback function is not sent 
+//    });
+
+
+//Callback Hell
+console.log("Calling Pavel --- ");
+getdata(61,"pavel",()=>{
+
+    console.log("Calling Tanvir --- ")
+    getdata(2,"Tanvir",()=>{
+
+        console.log("Calling Salman -- ")
+        getdata(102,"Salman",()=>{
+
+            console.log("Calling Nurullah -- ");
+            getdata(50,'Nurullah',()=>{
+
+                console.log("Calling Shimul --- ");
+                getdata(21,"Shimul");
+                
+            })
+        })
+    })
+})
