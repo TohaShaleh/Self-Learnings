42efe9637e30d9eaac555fa58d11ef9c4b4b0e5c
Abu Shaleh <abushalehtoha@gmail.com>
2024-08-07 00:37:37 +0600

    Added the Fore Each method of array

---
+++ JavaScript/Array_Methods/for_Each.js
@@ -0,0 +1,12 @@
+let arr=new Array();
+arr=['toha','afrin','priya apu','prema apu','anika apu'];
+
+
+
+arr.forEach((item,idx) => {
+     let a=item.toUpperCase();
+     console.log(`${a} is at : ${idx} `);
+
+    
+});
+
